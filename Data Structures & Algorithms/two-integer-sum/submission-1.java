88a62350ae276d7e639169989d04badc62e294f6
Victor Olatunji <volatunj@terpmail.umd.edu>
2026-05-26 14:53:04 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> num_map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (num_map.containsKey(complement)) {
+                return new int[] {num_map.get(complement), i};
+            }
+            
+            num_map.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException("no match found");
+
+
+
+
+
+
+
+
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = target - nums[i];
+
+                if (nums[j] == complement) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        throw new IllegalArgumentException("no match found");*/
+
+
+    }
+}
