32236cc411954ece6c3691552c058e24bb622ce6
Victor Olatunji <volatunj@terpmail.umd.edu>
2026-05-26 14:36:57 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) return false;
+
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+     
+        return Arrays.equals(arrS, arrT);
+    }
+}
